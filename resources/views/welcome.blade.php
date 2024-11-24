d590f2148e3d4cb4d3b06495b8c0317cc34f0336
Qeme_AX2 <qemeofficial07@gmail.com>
2024-11-24 18:15:00 +0800

    Lesson 2 - Routes & Views by using blade built-in functions, naming convention for using blade for each file, route paths and file config

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,5 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+{{-- this is called dynamic value inside lang (blade), if you remove the blade name in the file, this will return a string only (cant be used)--}}
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +9,7 @@
     <h1>Welcome to the Ninja Networks Tutorial</h1>
     <p>Click the button below to view the list of ninjas.</p>
 
-    <a href="" class="btn">
+    <a href="/ninjas" class="btn">
         Find Ninjas!
     </a>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* 
+routes differ depends on the url
+yourdomain.com/contact will bring user to contact page
+
+in laravel, after METHOD and the path, next is function to handle the request
+above cases, after the user get the /, it will return the page of "welcome" by using view() function built in inside laravel
+The laravel finds the name of the file inside views file, which one has the welcome name in it and execute the page
+*/
+
+Route::get('/ninjas',function(){
+    return view('ninjas.index');
+    // to enter the file name inside another directory, simply use .
+});
