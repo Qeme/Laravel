db03172ca673ca2185f77fab1d6a1ea5063a7af1
Qeme_AX2 <qemeofficial07@gmail.com>
2024-11-26 11:38:00 +0800

    Lesson 4 - Learn about Blade Directives like @if @endif @foreach @endforeach and so on

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -7,22 +7,24 @@
 <body>
     <h2>Currently Available Ninjas</h2>
 
+    {{-- impliment if statement/blade directives like react which render certain part if the if statement equals to true --}}
+    @if($greeting == "hi")
+        <p>Hi from inside the if statement</p>
+    @endif
+
     {{-- we want to pass dynamic value $ which will return a string hello --}}
     <p>{{ $greeting }}</p> 
 
     {{-- why not we put the array items inside the index page --}}
     <ul>
-        <li>
-            {{-- remember to always use dynamic value even in path --}}
-            <a href="/ninjas/{{$ninjas[0]["id"]}}">
-                {{ $ninjas[0]["name"] }}
-            </a>
-        </li>
-        <li>
-            <a href="/ninjas/{{$ninjas[1]["id"]}}">
-                {{ $ninjas[1]["name"] }}
-            </a>
-        </li>
+        {{-- first call for foreach directives and always close it --}}
+        @foreach($ninjas as $ninja)
+            <li>
+                <p>{{ $ninja["name"] }}</p>
+                <a href="/ninjas/{{ $ninja["id"] }}">View Details</a>
+            </li>
+        @endforeach
+        
     </ul>
-</body>
+</body> 
 </html>
