4761df23d5458c7800c2714d3d7625fc30b860e0
Qeme_AX2 <qemeofficial07@gmail.com>
2024-12-14 16:20:24 +0800

    Lesson 20 - Showing Form Errors by taking the ->all() and list them. We did also learn on how to save the wrong input value when the page load the error so the user can edit the data when needed

--- resources/views/ninjas/create.blade.php
+++ resources/views/ninjas/create.blade.php
@@ -1,5 +1,4 @@
 <x-layout>
-    <h2>Create New Ninjas</h2>
     <form action="{{ route('ninjas.store') }}" method="POST">
         {{-- add tag csrf Cross-Site Request Forgery (CSRF) Protection --}}
         @csrf
@@ -12,7 +11,7 @@
           type="text" 
           id="name" 
           name="name" 
-          value="{{ old('name') }}" 
+          value="{{ old('name') }}" {{-- we use odl() method to retrieve the previous data instead of null --}}
           required
         >
       
@@ -22,6 +21,7 @@
           type="number" 
           id="skill" 
           name="skill" 
+          value="{{ old('skill') }}"
           required
         >
       
@@ -30,9 +30,9 @@
         <textarea
           rows="5"
           id="bio" 
-          name="bio" 
+          name="bio"
           required
-        ></textarea>
+        >{{ old('bio') }}</textarea> {{-- the reason why no value= is because this is textarea which already an input value (closing tag)--}}
       
         <!-- select a dojo -->
         <label for="dojo_id">Dojo:</label>
@@ -40,7 +40,8 @@
           <option value="" disabled selected>Select a dojo</option>
           {{-- use foreach to iterate all the dojos --}}
           @foreach($dojos as $dojo)
-            <option value="{{$dojo->id}}">
+            {{-- now this is a bit complicated which we need to check the old value wheter it is same to the $dojo->id, if yes, makes it selected --}}
+            <option value="{{$dojo->id}}" {{ $dojo->id == old('dojo_id') ? 'selected' : '' }}>
                 {{$dojo->name}}
             </option>
           @endforeach
@@ -49,6 +50,15 @@
         <button type="submit" class="btn mt-4">Create Ninja</button>
       
         <!-- validation errors -->
-        
+        {{-- use the if which take any of the errors type --}}
+        @if ($errors->any())
+            <ul class="px-4 py-2 bg-red-100">
+                {{-- inside here, we loop all the errors if any; the reason we use $errors->all() not $errors because it is an object--}}
+                @foreach ($errors->all() as $error)
+                    <li class="my-2 text-red-500">{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+
       </form>
 </x-layout>
